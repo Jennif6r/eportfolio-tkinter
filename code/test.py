e51dcacc7a7ac9bb0232101bfa9ad5e8a0d6daf7
Jennifer <jennifer.zaminer@gmail.com>
2022-05-23 21:08:38 +0200

    add checkbox

--- code/test.py
+++ code/test.py
@@ -1,9 +1,7 @@
 import tkinter as tk
-from cgitb import text
-from tkinter import ttk  # neuere Style von Funktion getrennt
-import tkinter.font as tkFont
+from tkinter import ttk  # neuer Style von Funktion getrennt
+import tkinter.font as tkfont
 from tkinter.ttk import Radiobutton
-
 from PIL import Image, ImageTk  # für Bilder in Labels
 
 
@@ -12,32 +10,16 @@ def text_input():
 
 
 def set_option_label():
-    textoption.set(option.get())
-
-def 
+    textOption.set(option.get())
 
 
 root = tk.Tk()
 root.title('Python TKinter Beispiel Frame')
-root.geometry("350x400")  # Size
+root.geometry("350x450")  # Size
 root.minsize(width=250, height=250)  # min
 root.maxsize(width=800, height=800)  # max
 root.resizable(width=True, height=True)  # for no change of Size, set to False
-'''
-panel1 = tk.PanedWindow(root)
- panel1.configure(background='red')
- panel2 = tk.PanedWindow(root)
- panel2.configure(background='green')
- panel3 = tk.PanedWindow(root)
- panel3.configure(background='blue')
- panel4 = tk.PanedWindow(root)
- panel4.configure(background='yellow')
-
- panel1.pack(side='left')
- label = ttk.Label(panel1, text='TEST')
-panel1.add(label)
-'''
-font = tkFont.Font(size=20)
+font = tkfont.Font(size=20)
 
 # side: 'bottom','left', 'right'
 label1 = tk.Label(root, text='Textfeld', font=font)
@@ -64,39 +46,50 @@ radio2.grid(row=4, column=1)
 radio3 = Radiobutton(root, text='Option 3', value='option 3', variable=option, command=set_option_label)
 radio3.grid(row=4, column=2)
 
-textoption = tk.StringVar()
-textoption.set(' ')
-label4 = ttk.Label(root, textvariable=textoption)
+textOption = tk.StringVar()
+textOption.set(' ')
+label4 = ttk.Label(root, textvariable=textOption)
 label4.grid(row=6, columnspan=3)
 
 # image in frame
 label5 = ttk.Label(root, text='Bildbereich', font=font)
 label5.grid(row=7, columnspan=3)
-image = Image.open('th.jpg').resize(size=[120, 120])  # über .resize() Größe setzen
+image = Image.open('img/th.jpg').resize(size=(50, 50))  # über .resize() Größe setzen
 photo = ImageTk.PhotoImage(image)
 labelIm = ttk.Label(root, image=photo, compound='bottom')
 labelIm.grid(row=8, columnspan=3)
 
-# checkbox to choose one item
-label6 = ttk.Label(root, text='Auswahlbox')
+# combobox to choose one item
+textCombo = tk.StringVar()
+textCombo.set(' ')
+
+label6 = ttk.Label(root, text='Auswahlbox', font=font)
 label6.grid(row=9, columnspan=3)
-combo = ttk.Combobox(root, values=['Auswahl 1', 'Auswahl 2', 'Auswahl 3'])
-combo.grid(row=9, columnspan=3)
+combo = ttk.Combobox(root, values=['Auswahl 1', 'Auswahl 2', 'Auswahl 3'], textvariable=textCombo)
+combo.grid(row=10, columnspan=3)
 combo.current(0)
 combo.bind()
-textoptionCombo = tk.StringVar
-textoptionCombo.set(' ')
-label7 = ttk.Label(root, textvariable=textoptionCombo)
-label7.grid(row=10, columnspan=3)
 
-for item in label5.keys():
-    print(item, ': ', label5[item])
+label7 = ttk.Label(root, textvariable=textCombo)
+label7.grid(row=11, columnspan=3)
+
+# checkbox
+label8 = ttk.Label(root, text='Checkbox', font=font)
+label8.grid(row=12, columnspan=3)
+checkboxValue = tk.StringVar()
+checkboxValue.set(None)
+checkbox = tk.Checkbutton(root, text='Checkbox', variable=checkboxValue, onvalue=1, offvalue=0)
+checkbox.grid(row=13, columnspan=3)
+checkboxLabel = tk.Label(root, textvariable=checkboxValue)
+checkboxLabel.grid(row=14, columnspan=3)
+
+# for item in label5.keys():
+#     print(item, ': ', label5[item])
 
 # mit configure methode kann einiges geändert werden
 # Variable['Schlüssel'] können diese verändert werden
 # .keys() gibt mögliche Keys des Widgets
 
-#   # compound bestimmt Position relativ zum Text falls beides drin
-# labelIm.pack()
+# compound bestimmt Position relativ zum Text falls beides drin
 
 root.mainloop()
